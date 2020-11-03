27df21c7ef43ea72ccf5b8e54520859de80c10b6
adrianartale <23aartale@athenian.org>
2020-11-03 13:03:15 -0800

    finished

--- my_code.py
+++ my_code.py
@@ -5,13 +5,14 @@ sum_list = 0
 def avg_temp(sum_list):
     with open('temps.txt') as file_object:
         line_list = file_object.readlines()
-        list_length = len(line_list)
+        list_length = len(line_list) - 1
         for i in range(list_length):
             list_length = int(list_length)
         for i in range(1, len(line_list)):
             sum_list = sum_list + int(line_list[i])
         final_sum = sum_list/list_length
-    return final_sum
+        round_answer = round(final_sum,2)
+    return round_answer
 
 if __name__ == '__main__':
     print(avg_temp(sum_list))
--- my_test.py
+++ my_test.py
@@ -2,5 +2,5 @@
 from my_code import avg_temp
 
 
-def test_avg_temp():
-    assert 82.58 == avg_temp()
+def test_avg_temp(sum_list):
+    assert 82.58 == avg_temp(sum_list)
